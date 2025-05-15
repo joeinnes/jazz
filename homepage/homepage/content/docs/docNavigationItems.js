47e0c018b21c1b4b06e64ea2a03a5f661743f262
Trisha Lim <hello@trishalim.com>
2025-05-15 13:45:10 +0100

    rewording to follow diataxis

--- homepage/homepage/content/docs/docNavigationItems.js
+++ homepage/homepage/content/docs/docNavigationItems.js
@@ -265,7 +265,7 @@ export const docNavigationItems = [
     name: "Design patterns",
     items: [
       {
-        name: "Form",
+        name: "Autosaving forms",
         href: "/docs/design-patterns/form",
         done: 100,
       },
